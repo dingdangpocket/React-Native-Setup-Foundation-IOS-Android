26d92ec40bf87209f1acce4bb91ff47a412a4797
niegang <1003018791@qq.com>
2022-10-06 15:22:31 +0800

    visionCamera集成

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {Store} from './src/redux/store';
 import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['new NativeEventEmitter']); 
 
+
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 //核心库;
