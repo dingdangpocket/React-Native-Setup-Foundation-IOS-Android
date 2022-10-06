ceba982f168204cb25b45aeab7839405c786a27d
niegang <1003018791@qq.com>
2022-10-06 20:29:01 +0800

    webView集成

--- src/screens/home/TheoryDescScreen.js
+++ src/screens/home/TheoryDescScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {WebView} from 'react-native-webview';
 
 const TheoryDescScreen = () => {
+  const injectedJs = `window.alert("参数来自Native:id?10")`;
   return (
-    <View>
-      <ScrollView>
-        <Text style={{fontSize: 20}}>首页理论</Text>
-        <Text>作者:Dingdang</Text>
-      </ScrollView>
+    <View style={{flex:1}}>
+      <WebView
+        style={{flex:1}}
+        source={{uri: 'https://www.baidu.com'}}
+        injectedJavaScriptBeforeContentLoaded={injectedJs}
+        javaScriptEnabledAndroid={true}
+        javaScriptEnabled={true}
+        onMessage={event => {
+          alert('接收数据' + event.nativeEvent.data);
+        }}
+      />
     </View>
   );
 };
