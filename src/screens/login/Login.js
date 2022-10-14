855db890838500d224b4a9100b8386ad2bad020d
niegang <1003018791@qq.com>
2022-10-14 23:16:59 +0800

    登陆

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,15 +1,64 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
 const Login = () => {
     const navigation = useNavigation();
     return (
         <View style={{ flex: 1 }}>
-            <TouchableOpacity onPress={() => navigation.navigate("HomeTabs")}>
-                <Text style={{ fontSize: 16 }}>登陆页</Text>
-            </TouchableOpacity>
+            <View style={styles.mask}>
+                <View style={styles.loginContainer}>
+                </View>
+            </View>
         </View>
     );
 };
-const styles = StyleSheet.create({});
+//水平垂直居中;
+const Center = {
+    justifyContent: "center",
+    alignItems: "center",
+}
+//居中左对齐;
+const Left = {
+    justifyContent: "center",
+    alignItems: 'flex-start'
+}
+//居中右对齐;
+const Right = {
+    justifyContent: "center",
+    alignItems: 'flex-end'
+}
+
+//底部居中对齐;
+const BottomCenter = {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: 'flex-end'
+}
+
+//水平居中;
+const RowCenter = {
+    flexDirection: "row",
+    justifyContent: "center",
+}
+
+
+
+const styles = StyleSheet.create({
+    backgroudVideo: { width: 700, height: '100%' },
+    mask: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        ...Center
+    },
+    loginContainer: {
+        width: 100,
+        height: 100,
+        backgroundColor: "red",
+        borderRadius: 30,
+        ...Center,
+    }
+
+});
 export default Login;
