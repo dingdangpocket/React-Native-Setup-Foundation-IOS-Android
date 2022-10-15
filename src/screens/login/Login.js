3a4950c5a4fdb120b95be4d4efa99d58dcccefc2
niegang <1003018791@qq.com>
2022-10-15 19:05:58 +0800

    登陆页

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -3,67 +3,78 @@ import { useNavigation } from '@react-navigation/native';
 import { Center, RowCenter, Left, Right, BottomCenter, } from "../../commonStyle/commonStyle"
 import { useState } from 'react';
 import CustomButton from "../../components/CustomButton"
-
 const Login = () => {
     const navigation = useNavigation();
     const { width, height } = useWindowDimensions();
-    const [userName, onChangeUserName] = useState("");
+    const [phoneNum, onChangePhoneNum] = useState("");
+    const [accessCode, onChangeAccessCode] = useState("");
     const onLogin = () => {
+        console.log("onLoginParams", phoneNum, accessCode);
         navigation.navigate("HomeTabs")
     }
-    const onGetAccessCode = () => {};
+    const onGetAccessCode = () => { };
     return (
         <View style={{ flex: 1 }}>
             <View style={{
                 width: '100%',
                 height: '100%',
-                backgroundColor: 'white',
+                backgroundColor: 'rgba(10,10,10,0.9)',
                 ...Center
             }}>
                 <View style={{
-                    width: width * 0.95,
+                    width: width * 0.85,
                     height: 80,
                     borderRadius: 10,
                     flexDirection: "row",
                     ...Center,
+                    // backgroundColor:"red"
                 }}>
-                    <Text style={{ marginRight: 5 }}>+86</Text>
+                    <Text style={{ marginRight: 5, color: "white" }}>+86</Text>
                     <TextInput
-                        style={{ width: width * 0.8, borderBottomWidth: 2, borderColor: "rgba(10,10,10,0.3)" }}
-                        onChangeText={(value) => onChangeUserName(value)}
-                        value={userName}
+                        style={{ width: width * 0.7, borderBottomWidth: 2, borderColor: "rgba(255,255,255,0.8)", color: "white" }}
+                        onChangeText={(value) => {
+                            const authValue = value.replace(/[^\d]+/, '');
+                            onChangePhoneNum(authValue)
+                        }}
+                        value={phoneNum}
                         underlineColorAndroid="transparent"
                         placeholder="请输入手机号"
+                        placeholderTextColor={"white"}
                         keyboardType="number-pad"
                         maxLength={11}
                     />
                 </View>
                 <View style={{
-                    width: width * 0.95,
+                    width: width * 0.85,
                     height: 80,
                     borderRadius: 10,
                     flexDirection: "row",
-                    ...Center,
-
+                    justifyContent: "center",
+                    alignItems: "center",
+                    // backgroundColor:"red"
                 }}>
                     <TextInput
-                        style={{ width: width * 0.50, borderBottomWidth: 2, borderColor: "rgba(10,10,10,0.2)" }}
-                        onChangeText={(value) => onChangeUserName(value)}
-                        value={userName}
+                        style={{ width: width * 0.35, borderBottomWidth: 2, borderColor: "rgba(255,255,255,0.6)", color: "white" }}
+                        onChangeText={(value) => {
+                            const authValue = value.replace(/[^\d]+/, '');
+                            onChangeAccessCode(authValue)
+                        }}
+                        value={accessCode}
                         underlineColorAndroid="transparent"
                         placeholder="请输入验证码"
+                        placeholderTextColor={"white"}
                         keyboardType="number-pad"
-                        maxLength={11}
+                        maxLength={6}
                     />
                     <CustomButton
                         title="获取验证码"
                         titleColor="white"
                         fontSize={11}
                         width={width * 0.2}
-                        height={40}
-                        backgroundColor="#F73809"
+                        height={35}
+                        backgroundColor="rgba(255,51,0,0.6)"
                         borderRadius={2.5}
-                        marginLeft={10}
+                        marginLeft={15}
                         align={Center}
                         onPress={onGetAccessCode} />
                 </View>
@@ -71,9 +82,9 @@ const Login = () => {
                     title="登陆"
                     titleColor="white"
                     fontSize={18}
-                    width={width * 0.2}
+                    width={width * 0.85}
                     height={50}
-                    backgroundColor="#F73809"
+                    backgroundColor="rgba(255,51,0,0.7)"
                     borderRadius={2.5}
                     marginTop={10}
                     align={Center}
