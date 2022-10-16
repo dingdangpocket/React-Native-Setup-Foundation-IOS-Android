bcfc4b83d580e0ee3ae13801827dac2f30d446e0
niegang <1003018791@qq.com>
2022-10-16 12:06:11 +0800

    处理底部色彩兼容

--- src/screens/home/TheoryDescScreen.js
+++ src/screens/home/TheoryDescScreen.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
-import {WebView} from 'react-native-webview';
+import { View, StyleSheet} from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const TheoryDescScreen = () => {
   const injectedJs = `window.alert("参数来自Native:id?10")`;
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <WebView
-        style={{flex: 1}}
-        source={{uri: 'https://www.baidu.com'}}
+        style={{ flex: 1 }}
+        source={{ uri: 'https://www.baidu.com' }}
         injectedJavaScriptBeforeContentLoaded={injectedJs}
         javaScriptEnabledAndroid={true}
         javaScriptEnabled={true}
